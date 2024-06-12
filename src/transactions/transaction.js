c222866411fe4c04dcedb526050242d73e148fbc
luisher98 <luisheratm@gmail.com>
2024-06-12 01:34:05 +0100

    Refactor transaction fetching and sorting logic

--- src/transactions/transaction.js
+++ src/transactions/transaction.js
@@ -1,32 +1,7 @@
-import fs from "fs";
-import fetch from "node-fetch"; // Ensure you have node-fetch installed
 import { JSDOM } from "jsdom";
 
-const start = process.hrtime();
-
-export async function fetchPolititianTransactions(year) {
-  const requestOptions = {
-    method: "POST",
-    redirect: "follow",
-  };
-
-  try {
-    const response = await fetch(
-      `https://disclosures-clerk.house.gov/FinancialDisclosure/ViewMemberSearchResult?filingYear=${year}`,
-      requestOptions
-    );
-    const result = await response.text();
-    return result;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
-export async function fetchTransactionsByYear(year) {
-  const fetchStart = process.hrtime();
+export async function getTransactions(year) {
   const htmlData = await fetchPolititianTransactions(year);
-  const fetchEnd = process.hrtime(fetchStart);
 
   if (htmlData) {
     const dom = new JSDOM(htmlData);
@@ -61,34 +36,37 @@ export async function fetchTransactionsByYear(year) {
       });
     });
 
-    const sortStart = process.hrtime();
     sortByIdDescending(results);
-    const sortEnd = process.hrtime(sortStart);
 
-    fs.writeFileSync("transactions.json", JSON.stringify(results, null, 2)); // Convert results to JSON string
-
-    console.log(
-      `Fetch time: ${(fetchEnd[0] + fetchEnd[1] / 1e9).toFixed(3)} seconds`
-    );
-    console.log(
-      `Sort time: ${(sortEnd[0] + sortEnd[1] / 1e9).toFixed(3)} seconds`
-    );
+    return results;
   } else {
     console.error("Failed to fetch HTML data");
   }
 }
 
-function sortByIdDescending(array) {
-  return array.sort((a, b) => {
+async function fetchPolititianTransactions(year) {
+  const requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      `https://disclosures-clerk.house.gov/FinancialDisclosure/ViewMemberSearchResult?filingYear=${year}`,
+      requestOptions
+    );
+    const result = await response.text();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+function sortByIdDescending(obj) {
+  return obj.sort((a, b) => {
     if (a.id < b.id) return 1;
     if (a.id > b.id) return -1;
     return 0;
   });
 }
-
-fetchTransactionsByYear(2024).then(() => {
-  const end = process.hrtime(start);
-  console.log(
-    `Total execution time: ${(end[0] + end[1] / 1e9).toFixed(3)} seconds`
-  );
-});
